8124cf5f02fc2500e21649ff19cbda0ca553d78f
Dalton Smith <daltonsmith45@hotmail.com>
2020-08-06 18:02:24 -0500

    Update FPS Counter to show Current FPS rather than average & clean up some code

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ SDL_Renderer* renderer;
 SDL_Window* window;
 
 // FPS Variables
-int countedFrames = 0;
+int countedFrames, lastSecondTicks = 0;
 LTimer fps;
 LTimer cap;
 
@@ -30,13 +30,14 @@ void input() {
         if(e.type == SDL_QUIT) running = false;
     }
     
+    //TODO: Obviously needs much improvement, bit of a POC implementation
     if(keystates[SDL_SCANCODE_ESCAPE]) running = false;
     if(keystates[SDL_SCANCODE_F11]) fullscreen = !fullscreen;
 }
 
 void draw() {
     //Background Rendering
-    SDL_SetRenderDrawColor(renderer, 40, 43, 200, 255);
+    SDL_SetRenderDrawColor(renderer, 40, 43, 120, 155);
     SDL_Rect rect;
     rect.x=rect.y = 0;
     rect.w=WIDTH;
@@ -45,7 +46,7 @@ void draw() {
     //End Background Rendering
 
     //Keep track of number of frames
-    //TODO: Refactor
+    //TODO: Refactor into new method (& maybe create an FpsManager object? could clean things up)
     if(cap.getTicks() < SCREEN_TICKS_PER_FRAME)
     {
         //Sleep the remaining frame time
@@ -56,14 +57,26 @@ void draw() {
     countedFrames++;
 }
 
+void __init__() {
+    //TODO: More robust error handling system
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) std::cout << "Failed to initialize SDL..." << std::endl;
+    if(SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer) < 0) std::cout << "Failed to create SDL Window or Renderer" << std::endl;
+    SDL_SetWindowTitle(window, "Halcyon Plains");
+    SDL_ShowCursor(1);
+}
+
+void __destroy__() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
 int main(int argc, char* args[]) {
+    int ticks;
     running = true;
     fullscreen = false;
 
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) std::cout << "Failed to initialize SDL..." << std::endl;
-    if(SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer) < 0) std::cout << "Failed to create SDL Window or Renderer" << std::endl;
-    SDL_SetWindowTitle(window, "SDL Test Game");
-    SDL_ShowCursor(1);
+    __init__();
 
     fps.start();
 
@@ -71,17 +84,19 @@ int main(int argc, char* args[]) {
 
         cap.start();
 
-        //Calculate and correct fps
-        float avgFPS = countedFrames / ( fps.getTicks() / 1000.f );
-        std::cout << "Frames per Second: " << avgFPS << std::endl;
+        ticks = fps.getTicks();
+        //Calculate current fps
+        if(ticks > (lastSecondTicks+1000)) {
+            lastSecondTicks = ticks;
+            std::cout << "Frames Per Second: " << countedFrames << std::endl;
+            countedFrames = 0;
+        }
 
         update();
         input();
         draw();
     }
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    __destroy__();
     return 0;
 }
